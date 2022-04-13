05bfc2af73a9c793f793151441713aa8e73fb4c2
unknown <enzolaunaygarnier@gmail.com>
2022-04-13 18:06:23 +0200

    commit

--- bot.py
+++ bot.py
@@ -1,19 +1,48 @@
 from ctypes import resize
-import tickets
+
+from numpy import append
+from tickets import Tickets
 import discord
 from discord.ext import commands
 import asyncio
 import time
 import random
 
-normal = tickets.Tickets("normal", 30)
-plus = tickets.Tickets("plus", 15)
-premium = tickets.Tickets("premium", 5)
+
+
+normal = Tickets("normal", 30, 100)
+plus = Tickets("plus", 15, 450)
+premium =Tickets("premium", 5, 1000)
 
 
 
 token = "OTU5OTkxODQ5NjAxMzUxNzIy.Ykj8FA.-YesWwQNdltUozW8GhpSRBd3xDk"
 bot = commands.Bot(command_prefix="?")
+
+
+
+data = {
+
+    #id                 #wallet
+    299956001573044224 : 0,
+    
+
+    
+    }
+
+async def check_id(ctx, id):
+    
+    
+    if id not in data:
+        print(data)
+        data[id] = 0
+        await ctx.send(data[id])
+
+
+
+
+
+
 
 
 @bot.event
@@ -28,31 +57,33 @@ async def test(ctx, what):
 
 
 
+
+
 @bot.command()
 async def buy(ctx, ticket):
     
     list_tickets= ["normal","plus", "premium"]
     
     author = ctx.message.author.mention
-    message = await ctx.send(f"{author} a lancé la machine...")
+    id = ctx.message.author.id
     
+ 
+    await check_id(ctx, id)
     if ticket not in list_tickets:
         await buy.error()
+    
     elif ticket == "normal":
         result = normal.play()
+        data[id] += normal.price
+    
     elif ticket == "plus":
         result = plus.play()
+        data[id] += plus.price
+    
     elif ticket == "premium":
         result = premium.play()
-        
-    
-        
-        
-       
-        
-        
-        
-    
+        data[id] += premium.price
+    message = await ctx.send(f"{author} a lancé la machine...")
     
     
     if result == True:
@@ -65,11 +96,7 @@ async def buy(ctx, ticket):
             await asyncio.sleep(0.5)
             await message.edit(content="⭐⭐⭐")
             await asyncio.sleep(0.5)
-            await ctx.send("Vous avez gagné ! 🤩")
-    
-    
-    
-    
+            await ctx.send(f"{author} tu as gagné ! 🤩")
     else:
         
         
@@ -86,21 +113,135 @@ async def buy(ctx, ticket):
         await asyncio.sleep(0.5)
         await message.edit(content=f"{list_emoji[0]}{list_emoji[1]}{list_emoji[2]}")
         await asyncio.sleep(0.5)
-                
-                
-                
-        await ctx.send("Vous avez perdu ! :pensive:")
+        await ctx.send(f"{author} tu as perdu... :pensive:")
+    
+
+    
+@buy.error
+async def on_command_error(ctx, error):
+    await ctx.send("""```Erreur de syntaxe\n
+syntaxe :!buy   <ticket>\n   
+                normal\n
+                plus\n
+                premium```""")
+
+
+
+
+
+
+@bot.command(aliases=["debt"])
+async def dette(ctx, member: discord.Member=""):
     
     
     
-    # await ctx.send(author)
     
     
     
+    if member == "":
+        member = ctx.message.author
+        await check_id(ctx, ctx.message.author.id)
+        member_wallet = data[ctx.message.author.id]
+        if member_wallet > 0:
+            await ctx.send(f"Votre dette s'élève à {member_wallet} G 🪙")
+        elif member_wallet == 0:
+            await ctx.send(f"Vous n'avez aucune dette ! 🪙")
+        elif member_wallet < 0:
+            await ctx.send(f"Le serveur vous doit {'{:}'.format(abs(member_wallet))} G 🪙")
+
+            
+            
+    else:
+        await check_id(ctx, member.id)
+        member_wallet = data[member.id]
+        
+        
+        if member_wallet > 0:
+            await ctx.send(f"la dette de {member.mention} est de {member_wallet} G 🪙")
+        elif member_wallet == 0:
+            await ctx.send(f"La dette de {member.mention} s'élève à {member_wallet} G 🪙")
+        elif member_wallet < 0:
+            await ctx.send(f"Le serveur doit à {member.mention} {'{:}'.format(abs(member_wallet))} G 🪙")
     
-# @buy.error
-# async def on_command_error(ctx, error):
-#     await ctx.send("ERROR")
+    
+    
+
+
+@bot.command(aliases=["probas"])
+async def proba(ctx, *, what):
+    await ctx.reply(f"les probabilités sont de {random.randint(0,100)}%")
+    
+    
+
+
+
+
+
+
+
+
+
+
+pute_list = []
+@bot.command()
+async def pute(ctx, member:discord.Member):
+    
+    if str(member) not in pute_list:
+        await ctx.reply(f"{member.mention} a été ajouté à la liste des putes du serveur avec succès ✅")
+        pute_list.append(str(member))
+    else:
+        await ctx.reply(f"{member.mention} est déjà dans la liste des putes du serveur")
+    
+
+
+@bot.command(aliases=["putelist","puteliste","listepute","putelistes"])
+async def listputes(ctx):
+    
+    
+
+
+    x = "\n".join(pute_list)
+
+
+    await ctx.reply(f"liste des putes : \n{x}")
+    
+
+
+
+
+@bot.command(aliases=["pay"])
+async def refund(ctx, member:discord.Member, amount):
+    if amount == "all":
+        if data[member.id] < 0:
+            await ctx.reply("L'utilisateur est déjà en positif !")
+        elif data[member.id] > 0:
+            data[member.id] = 0
+            await ctx.reply(f"La dette de {member} a été remise à 0 🪙")
+        
+    else:
+        try:
+            int(amount)
+
+        except(ValueError):
+            await ctx.reply("Montant non reconnu. Réessayez")
+        
+        else:
+            data[member.id] = data[member.id] - int(amount)
+            await ctx.reply(f"La dette de {member} a été réduite de {amount} G 🪙")
+            
+        
+        
+        
+            
+    
+            
+        
+    
+    
+
+
+
+
 
 
 
--- tickets.py
+++ tickets.py
@@ -1,19 +1,20 @@
 import random
 
 class Tickets():
-    def __init__(self, name, probability) -> None:
+    def __init__(self, name, probability, price) -> None:
         """fonction utilisé pour faire un mini jeu de casino
 
         Args:
             name (str): nom du ticket
             probability (int): probabilité de gagner; exemple : probability = 10; = 1/10
+            price (int) : prix du ticket
         """        
         
         
         
         self.name = name
         self.probability = probability
- 
+        self.price = price
     
     
     def play(self):
@@ -24,4 +25,10 @@ class Tickets():
             return True
         else:
             return False
+        
+    
+    
+    def remove_money(self, account, amount):
+        account = account - amount
+        
         
